a03eb5b7b0f477680d4d8238a022cc9ff8651994
daxxmo <45502046+daxxmo@users.noreply.github.com>
2019-05-18 12:20:54 -0600

    Source packages 2

---
+++ proyectoEstructuraDatos(Alejo,Milton)/src/java/dbAction/indexDbAction.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbAction;
+
+/**
+ *
+ * @author GALLO
+ */
+public class indexDbAction {
+    
+}
