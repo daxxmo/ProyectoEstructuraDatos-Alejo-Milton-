7a9d487944389a86b0addc7a2390d53d0f36aef8
Milton Perez <perezmilton@gmail.com>
2019-06-13 23:49:16 -0600

    imprimir carnet

--- proyectoEstructuraDatos(Alejo,Milton)/src/java/bean/EditarAlumnoBean.java
+++ proyectoEstructuraDatos(Alejo,Milton)/src/java/bean/EditarAlumnoBean.java
@@ -19,7 +19,7 @@ class EditarAlumnoBean {
 
 
     EditarAlumnoBean() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+     
     }
   public String regresarMenu(){
         return "regresarMenu";
